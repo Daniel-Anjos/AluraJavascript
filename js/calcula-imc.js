1b56179002bbbb0358082c89f467b94cedada729
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2019-05-09 21:13:13 -0300

    Ajuste na organização do código

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -38,34 +38,7 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function () {
-    event.preventDefault();
-    var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-})
-
-
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura*altura);
+}
