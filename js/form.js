cb077825cd0b89b387d04616bd92d0f1daea43a3
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2019-05-09 22:34:55 -0300

    Otimizando a divisão do código e criando novas funções

--- js/form.js
+++ js/form.js
@@ -18,11 +18,13 @@ botaoAdicionar.addEventListener("click", function () {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
